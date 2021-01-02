9194349dc1db2035368bd216f010929fac6cd5ec
codychen1005 <53663918+codychen1005@users.noreply.github.com>
2021-01-02 18:35:00 -0500

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Header from './Header';
+import Particles from "react-particles-js";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+    particlesCanva: {
+        position: "absolute"
+    }
+});
 
 const Home = () => {
+    const classes = useStyles();
     return (
-        <div>
+        <>
             <Navbar/>
-        </div>
+            <Header/>
+            <Particles
+                canvasClassName ={classes.particlesCanva}
+                params = {{
+                    number:{
+                        value: 50,
+                        density: {
+                            enable: true,
+                            value_area: 900
+                        }
+                    },
+                    shape: {
+                        type: "star",
+                        stroke: {
+                            width: 1,
+                            color: "tomato"
+                        }
+                    },
+                    size: {
+                        value: 8,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 5,
+                            size_min: 0.1,
+                            sync: true
+                        }
+                    },
+                    opacity: {
+                        value: 1,
+                        random: true,
+                        anim:{
+                            enable:true,
+                            speed: 1,
+                            opacity_min: 0.1,
+                            sync: true
+                        }
+                    }
+                    
+                }}
+            />
+
+        </>
     );
 };
 
