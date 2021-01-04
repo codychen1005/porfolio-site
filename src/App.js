6ad9e24bc1ddd434eed85493b309914088617d48
codychen1005 <53663918+codychen1005@users.noreply.github.com>
2021-01-04 18:18:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import './App.css';
-import Home from "./components/";
+import {Route} from "react-router-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Home from "./components/";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
+import Cover from "./components/Cover";
 
 function App() {
     return (
       <>
         <CssBaseline/>
-          <Home/>
+        <Route exact path="/" component ={Home}/>
+        <Route path="/resume" component ={Resume}/>
+        <Route path="/cover" component ={Cover}/>
+        <Route path="/projects" component ={Projects}/>
       </>
       );  
 }
