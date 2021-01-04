628d50382894a92ec4233cbc680eb01b778bd2ac
codychen1005 <53663918+codychen1005@users.noreply.github.com>
2021-01-04 18:18:24 -0500

    Create Cover.js

---
+++ src/components/Cover.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Cover = () => {
+    return (
+        <>
+            <Navbar />
+            This is the resume components
+        </>
+    )
+}
+
+export default Cover
