6d35f59118f30f2a40bb2402e0bebd0f0a6ee70a
codychen1005 <53663918+codychen1005@users.noreply.github.com>
2021-01-04 18:18:36 -0500

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,8 @@ import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles({
     particlesCanva: {
-        position: "absolute"
+        position: "absolute",
+        opacity: "0.5"
     }
 });
 
@@ -20,21 +21,144 @@ const Home = () => {
                 canvasClassName ={classes.particlesCanva}
                 params = {{
                     number:{
-                        value: 50,
+                        value: 1000,
                         density: {
                             enable: true,
-                            value_area: 900
+                            value_area: 2000
                         }
                     },
                     shape: {
-                        type: "star",
+                        type: 'circle',
                         stroke: {
-                            width: 1,
+                            width: 100,
                             color: "tomato"
                         }
                     },
                     size: {
-                        value: 8,
+                        value: 80,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 5,
+                            size_min: 0.1,
+                            sync: true
+                        }
+                    },
+                    opacity: {
+                        value: 1,
+                        random: true,
+                        anim:{
+                            enable:true,
+                            speed: 1,
+                            opacity_min: 0.1,
+                            sync: true
+                        }
+                    }
+                    
+                }}
+            />
+            <Particles
+                canvasClassName ={classes.particlesCanva}
+                params = {{
+                    number:{
+                        value: 1000,
+                        density: {
+                            enable: true,
+                            value_area: 2000
+                        }
+                    },
+                    shape: {
+                        type: 'circle',
+                        stroke: {
+                            width: 100,
+                            color: "tomato"
+                        }
+                    },
+                    size: {
+                        value: 80,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 5,
+                            size_min: 0.1,
+                            sync: true
+                        }
+                    },
+                    opacity: {
+                        value: 1,
+                        random: true,
+                        anim:{
+                            enable:true,
+                            speed: 1,
+                            opacity_min: 0.1,
+                            sync: true
+                        }
+                    }
+                    
+                    
+                }}
+            />
+
+                <Particles
+                canvasClassName ={classes.particlesCanva}
+                params = {{
+                    number:{
+                        value: 1000,
+                        density: {
+                            enable: true,
+                            value_area: 2000
+                        }
+                    },
+                    shape: {
+                        type: 'circle',
+                        stroke: {
+                            width: 100,
+                            color: "tomato"
+                        }
+                    },
+                    size: {
+                        value: 80,
+                        random: true,
+                        anim: {
+                            enable: true,
+                            speed: 5,
+                            size_min: 0.1,
+                            sync: true
+                        }
+                    },
+                    opacity: {
+                        value: 1,
+                        random: true,
+                        anim:{
+                            enable:true,
+                            speed: 1,
+                            opacity_min: 0.1,
+                            sync: true
+                        }
+                    }
+                    
+                }}
+            />
+
+                <Particles
+                canvasClassName ={classes.particlesCanva}
+                params = {{
+                    number:{
+                        value: 1000,
+                        density: {
+                            enable: true,
+                            value_area: 2000
+                        }
+                    },
+                    shape: {
+                        type: 'circle',
+                        stroke: {
+                            width: 100,
+                            color: "tomato"
+                        }
+                    },
+                    size: {
+                        value: 80,
                         random: true,
                         anim: {
                             enable: true,
