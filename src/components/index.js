1d8570d89fcc33a21ed385702dc2aa400c995e82
codychen1005 <53663918+codychen1005@users.noreply.github.com>
2021-01-04 21:28:27 -0500

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -18,166 +18,173 @@ const Home = () => {
             <Navbar/>
             <Header/>
             <Particles
-                canvasClassName ={classes.particlesCanva}
-                params = {{
-                    number:{
-                        value: 1000,
-                        density: {
-                            enable: true,
-                            value_area: 2000
-                        }
-                    },
-                    shape: {
-                        type: 'circle',
-                        stroke: {
-                            width: 100,
-                            color: "tomato"
-                        }
-                    },
-                    size: {
-                        value: 80,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 5,
-                            size_min: 0.1,
-                            sync: true
-                        }
-                    },
-                    opacity: {
-                        value: 1,
-                        random: true,
-                        anim:{
-                            enable:true,
-                            speed: 1,
-                            opacity_min: 0.1,
-                            sync: true
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles: {
+                        number:{
+                            value: 45,
+                            density:{
+                                enable: true,
+                                value_area: 900
+                            }
+                        },
+                        shape:{
+                            type: "star",
+                            stroke:{
+                                width: 1,
+                                color: "tomato"
+                            }
+                        },
+                        size:{
+                            value: 8,
+                            random: true,
+                            anim:{
+                                enable: true,
+                                speed: 8,
+                                size_min: 0.1,
+                                sync: true
+                            }
+                        },
+                        opacity:{
+                            value: 1,
+                            random: true,
+                            anim:{
+                                enable: true,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: true
+                            }
                         }
                     }
-                    
+
                 }}
-            />
+            /> 
             <Particles
-                canvasClassName ={classes.particlesCanva}
-                params = {{
-                    number:{
-                        value: 1000,
-                        density: {
-                            enable: true,
-                            value_area: 2000
-                        }
-                    },
-                    shape: {
-                        type: 'circle',
-                        stroke: {
-                            width: 100,
-                            color: "tomato"
-                        }
-                    },
-                    size: {
-                        value: 80,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 5,
-                            size_min: 0.1,
-                            sync: true
-                        }
-                    },
-                    opacity: {
-                        value: 1,
-                        random: true,
-                        anim:{
-                            enable:true,
-                            speed: 1,
-                            opacity_min: 0.1,
-                            sync: true
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles: {
+                        number:{
+                            value: 45,
+                            density:{
+                                enable: true,
+                                value_area: 900
+                            }
+                        },
+                        shape:{
+                            type: "star",
+                            stroke:{
+                                width: 1,
+                                color: "tomato"
+                            }
+                        },
+                        size:{
+                            value: 8,
+                            random: true,
+                            anim:{
+                                enable: true,
+                                speed: 8,
+                                size_min: 0.1,
+                                sync: true
+                            }
+                        },
+                        opacity:{
+                            value: 1,
+                            random: true,
+                            anim:{
+                                enable: true,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: true
+                            }
                         }
                     }
-                    
-                    
+
                 }}
             />
 
-                <Particles
-                canvasClassName ={classes.particlesCanva}
-                params = {{
-                    number:{
-                        value: 1000,
-                        density: {
-                            enable: true,
-                            value_area: 2000
-                        }
-                    },
-                    shape: {
-                        type: 'circle',
-                        stroke: {
-                            width: 100,
-                            color: "tomato"
-                        }
-                    },
-                    size: {
-                        value: 80,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 5,
-                            size_min: 0.1,
-                            sync: true
-                        }
-                    },
-                    opacity: {
-                        value: 1,
-                        random: true,
-                        anim:{
-                            enable:true,
-                            speed: 1,
-                            opacity_min: 0.1,
-                            sync: true
+<Particles
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles: {
+                        number:{
+                            value: 45,
+                            density:{
+                                enable: true,
+                                value_area: 900
+                            }
+                        },
+                        shape:{
+                            type: "star",
+                            stroke:{
+                                width: 1,
+                                color: "tomato"
+                            }
+                        },
+                        size:{
+                            value: 8,
+                            random: true,
+                            anim:{
+                                enable: true,
+                                speed: 8,
+                                size_min: 0.1,
+                                sync: true
+                            }
+                        },
+                        opacity:{
+                            value: 1,
+                            random: true,
+                            anim:{
+                                enable: true,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: true
+                            }
                         }
                     }
-                    
+
                 }}
             />
 
-                <Particles
-                canvasClassName ={classes.particlesCanva}
-                params = {{
-                    number:{
-                        value: 1000,
-                        density: {
-                            enable: true,
-                            value_area: 2000
-                        }
-                    },
-                    shape: {
-                        type: 'circle',
-                        stroke: {
-                            width: 100,
-                            color: "tomato"
-                        }
-                    },
-                    size: {
-                        value: 80,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 5,
-                            size_min: 0.1,
-                            sync: true
-                        }
-                    },
-                    opacity: {
-                        value: 1,
-                        random: true,
-                        anim:{
-                            enable:true,
-                            speed: 1,
-                            opacity_min: 0.1,
-                            sync: true
+<Particles
+                canvasClassName={classes.particlesCanva}
+                params={{
+                    particles: {
+                        number:{
+                            value: 45,
+                            density:{
+                                enable: true,
+                                value_area: 900
+                            }
+                        },
+                        shape:{
+                            type: "star",
+                            stroke:{
+                                width: 1,
+                                color: "tomato"
+                            }
+                        },
+                        size:{
+                            value: 8,
+                            random: true,
+                            anim:{
+                                enable: true,
+                                speed: 8,
+                                size_min: 0.1,
+                                sync: true
+                            }
+                        },
+                        opacity:{
+                            value: 1,
+                            random: true,
+                            anim:{
+                                enable: true,
+                                speed: 1,
+                                opacity_min: 0.1,
+                                sync: true
+                            }
                         }
                     }
-                    
+
                 }}
             />
 
