46124c9d3ff2a2d813f258ff1a5cd3cd68ba0165
codychen1005 <53663918+codychen1005@users.noreply.github.com>
2021-01-05 20:44:20 -0500

    Update Cover.js

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import {Button} from "@material-ui/core";
+import MyResume from "../CV/Weihong_Resume.pdf";
 
 const Cover = () => {
     return (
         <>
-            <Navbar />
-            This is the resume components
+        <Navbar />
+        <a href= {MyResume} target="_blank" rel="noreferrer">
+            Click me            
+        </a>
         </>
     )
 }
